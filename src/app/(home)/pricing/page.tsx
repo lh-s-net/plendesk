20d09f4b72ad3b24e7180f563c2dd2bc985627a7
JW <info@lh-s.net>
2025-07-23 19:02:52 +0200

    Add pricing page, update middleware, and enhance home layout

---
+++ src/app/(home)/pricing/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Image from "next/image";
+import { dark } from "@clerk/themes";
+import { PricingTable } from "@clerk/nextjs";
+import {useTheme} from "next-themes";
+
+const Page = () => {
+  const currentTheme = useTheme().theme || "light"
+
+  return (
+    <div className="flex flex-col max-w-3xl mx-auto w-full">
+      <section className="space-y-6 pt-10 2xl:pt-48">
+        <div className="flex flex-col items-center">
+          <Image
+            src="/globe.svg"
+            alt="Vibe"
+            width={50}
+            height={50}
+            className="hidden md:block"
+          />
+        </div>
+        <h1 className="text-xl md:text-3xl font-bold text-center">Pricing</h1>
+        <p className="text-muted-foreground text-center text-sm md:text-base">
+          Choose the plan that fits your needs
+        </p>
+        <PricingTable
+          appearance={{
+            baseTheme: currentTheme === "dark" ? dark : undefined,
+            elements: {
+              pricingTableCard: "border! shadow-none! rounded-lg!"
+            }
+          }}
+        />
+      </section>
+    </div>
+   );
+}
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import {PricingTable} from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <div className="max-w-5xl mx-auto w-full flex justify-between items-center">
-      Hello PlenDesk.com
+    <div className="max-w-5xl mx-auto my-10 w-full flex flex-col justify-center items-start space-y-4">
+      <p>Hello PlenDesk!!!</p>
     </div>
   );
 }
