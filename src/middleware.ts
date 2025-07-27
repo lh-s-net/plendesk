4c365893853874165c323d507fca69eb70d11b2e
JW <info@lh-s.net>
2025-07-27 15:35:06 +0200

    Remove `.idea` folder and related files from version control

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,9 @@
 import {clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher([
-  "/(.*)",
+  "/",
+  "/sw.js",
+  "/manifest.json",
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/pricing(.*)",
