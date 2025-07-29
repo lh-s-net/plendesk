cd86466252d5bd56a5a7135273de68faa5085650
JW <info@lh-s.net>
2025-07-29 20:33:24 +0200

    Update navigation logo and restructure icons for consistency and maintenance

--- src/app/components/layout-main/nav-main.tsx
+++ src/app/components/layout-main/nav-main.tsx
@@ -17,7 +17,7 @@ export default function NavMain() {
       className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center justify-between max-w-7xl px-4 sm:px-6 lg:px-8">
         <Link href="/" className="flex items-center gap-2 font-semibold">
-          <Image src="/icons/96.png" alt="PlenDesk Logo" width={32} height={32}/>
+          <Image src="/icons/icon-96x96.png" alt="PlenDesk Logo" width={32} height={32}/>
           <span className="hidden sm:inline-block">PlenDesk</span>
         </Link>
 
