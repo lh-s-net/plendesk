61cd9f61241770142ec3d681f42a34a8d65d0e58
JW <info@lh-s.net>
2025-11-26 20:04:27 +0100

    updaate app carusell

--- src/convex/auth.config.ts
+++ src/convex/auth.config.ts
@@ -1,4 +1,4 @@
-export default {
+const authConfig = {
   providers: [
     {
       domain: "https://cute-jaguar-95.clerk.accounts.dev",
@@ -6,3 +6,5 @@ export default {
     },
   ]
 };
+
+export default authConfig;
